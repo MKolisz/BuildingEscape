884368d61a01c8cf27089f5917f1b1b2de439b04
DESKTOP-S1IQFSU\mkolisz11 <mkolisz11@gmail.com>
2019-03-01 00:20:00 +0100

    Using player controller, collision volumes, GetTimeSeconds to make door opens and closes after stepping in/out of the trigger volume

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/World.h"
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h" //always muust be the last include
 
@@ -22,6 +22,7 @@ protected:
 	virtual void BeginPlay() override;
 
 	void OpenDoor();
+	void CloseDoor();
 
 public:	
 	// Called every frame
@@ -29,12 +30,18 @@ public:
 
 
 private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(EditAnywhere)
 	float OpenAngle = 90.f;
 
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume *PressurePlate;
 	
 	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 0.5f;
+
+	float LastDoorOpenTime;
+
 	AActor *ActorThatOpens; //remember that pawn inherits from actor, mozna by uzyc APawn zamiast AActor
+
+	AActor *Owner; //the owning door
 };
