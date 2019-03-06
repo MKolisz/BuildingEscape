b3f745939fa87ae4e6c226ea3728cf982e02eede
DESKTOP-S1IQFSU\mkolisz11 <mkolisz11@gmail.com>
2019-03-06 01:35:36 +0100

    Modified default pawn actor, inherited game mode blueprint, getting player viewpoint and using DrawDebugLine

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/World.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
+#include "Grabber.generated.h"
+
+#define OUT
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	float Reach = 100.f; //how far ahead of the player can we reach in cm
+	
+};
